3b0a3dd305374f08ba59024296c85ba731aa564f
Bob Fornal <bob.fornal@leadingedje.com>
2023-02-07 12:56:33 -0500

    Regex Validation

--- src/app/core/services/_chatgpt.regex-validation.service.spec.ts
+++ src/app/core/services/_chatgpt.regex-validation.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { EmailValidationService } from './email-validation.service';
+import { RegexValidationService } from './regex-validation.service';
 
 const validEmails = [
   "user@example.com",
@@ -26,12 +26,25 @@ const invalidEmails = [
   "user@exam-ple.com.",
 ];
 
-describe('EmailValidationService', () => {
-  let service: EmailValidationService;
+const validDeaNumbers = [
+  'a0000000',
+  'z9123456',
+  'A0000007'
+];
+const invalidDeaNumbers = [
+  'A00000000',
+  '0A000000',
+  '000000A',
+  'A000000',
+  'a000000',
+];
+
+describe('RegexValidationService', () => {
+  let service: RegexValidationService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(EmailValidationService);
+    service = TestBed.inject(RegexValidationService);
   });
 
   it('should be created', () => {
@@ -49,6 +62,35 @@ describe('EmailValidationService', () => {
     const emailRegex = service.comprehensiveRegex;
     invalidEmails.forEach((email) => {
       expect(emailRegex.test(email)).toEqual(false);
+    });
+  });
+});
+
+describe('Regex pattern validation', function() {
+  let service: RegexValidationService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(RegexValidationService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should return true for valid inputs', function() {
+    const deaRegex = service.deaRegex;
+
+    validDeaNumbers.forEach(function(input) {
+      expect(deaRegex.test(input)).toBe(true);
+    });
+  });
+
+  it('should return false for invalid inputs', function() {
+    const deaRegex = service.deaRegex;
+
+    invalidDeaNumbers.forEach(function(input) {
+      expect(deaRegex.test(input)).toBe(false);
     });
   });
 });
--- src/app/core/services/regex-validation.service.ts
+++ src/app/core/services/regex-validation.service.ts
@@ -3,11 +3,11 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class EmailValidationService {
+export class RegexValidationService {
 
   basicRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
   comprehensiveRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-
+  deaRegex = /^[a-zA-Z][a-zA-Z0-9][0-9]{6}$/;
 
   constructor() { }
 }
